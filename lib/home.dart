9807e3b5455a0c1a194cae05080ad96e22883958
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-25 08:59:21 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:supernova/memory.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Wrap(
         alignment: WrapAlignment.center,
         crossAxisAlignment: WrapCrossAlignment.center,
         runAlignment: WrapAlignment.center,
         spacing: 10,
         runSpacing: 10,
-        children: <Widget>[],
+        children: <Widget>[Memory()],
       ),
     );
   }
