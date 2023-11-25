6d7019579177e9e6fa083c87c00c4859ec977161
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-25 09:29:15 +0100

    Modified  lib/memory.dart

--- lib/memory.dart
+++ lib/memory.dart
@@ -79,7 +79,7 @@ class _MemoryState extends State<Memory> {
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
                     const SizedBox(height: 10),
-                    Text("Available : $_memory", style: TextStyle(fontSize: 14.sp)),
+                    Text("Available : $_memory", style: TextStyle(fontSize: 13.sp)),
                     const SizedBox(height: 10),
                   ],
                 );
