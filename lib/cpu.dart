21ebe64c42ce13834736d7463fa2bfd48dfc4fdc
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-25 11:20:03 +0100

    Modified  lib/cpu.dart

--- lib/cpu.dart
+++ lib/cpu.dart
@@ -75,7 +75,7 @@ class _MemoryState extends State<CPU> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisSize: MainAxisSize.min,
                   children: <Widget>[
-                    Text("Available : $_clock MHz", style: const TextStyle(fontSize: 13)),
+                    Text("$_clock MHz", style: const TextStyle(fontSize: 13)),
                     const SizedBox(height: 20),
                   ],
                 );
