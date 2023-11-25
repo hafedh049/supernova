bf8566f11ec0e9672eb9b028ccb3c8f04ff7993a
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-25 08:53:48 +0100

    ModifiedM lib/memory.dart

--- lib/memory.dart
+++ lib/memory.dart
@@ -10,6 +10,8 @@ class Memory extends StatefulWidget {
 class _MemoryState extends State<Memory> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(),
+    );
   }
 }
