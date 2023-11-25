9b4479c02d971379dbdbc506e57a9126bea4ddf7
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-25 08:53:27 +0100

    AddedM lib/memory.dart

---
+++ lib/memory.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Memory extends StatefulWidget {
+  const Memory({super.key});
+
+  @override
+  State<Memory> createState() => _MemoryState();
+}
+
+class _MemoryState extends State<Memory> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
