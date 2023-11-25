aaeba75e0ffafc33f5f9b320f099094ee6533922
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-25 09:48:14 +0100

    Modified  lib/memory.dart

--- lib/memory.dart
+++ lib/memory.dart
@@ -91,7 +91,7 @@ class _MemoryState extends State<Memory> {
                     const SizedBox(height: 10),
                     Row(
                       children: <Widget>[
-                        for (int index = 1; index < 14; index++) ...<Widget>[
+                        for (bool slot in _slots) ...<Widget>[
                           Container(
                             decoration: BoxDecoration(
                               shape: BoxShape.circle,
