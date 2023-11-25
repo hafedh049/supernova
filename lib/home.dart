9ae848b5b58765496ab4d364a3fc7468eb0f1181
Hafedh Gunichi <120606876+hafedh049@users.noreply.github.com>
2023-11-25 09:03:12 +0100

    Modified  lib/home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -7,13 +7,17 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Wrap(
-        alignment: WrapAlignment.center,
-        crossAxisAlignment: WrapCrossAlignment.center,
-        runAlignment: WrapAlignment.center,
-        spacing: 10,
-        runSpacing: 10,
-        children: <Widget>[Memory()],
+      body: Center(
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.center,
+          runAlignment: WrapAlignment.center,
+          spacing: 10,
+          runSpacing: 10,
+          children: <Widget>[
+            Memory(),
+          ],
+        ),
       ),
     );
   }
